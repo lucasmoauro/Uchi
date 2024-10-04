1585f324ab665c81a53e65e761a0515aa9152f93
Lucas Moauro <lmoauro6@gmail.com>
2024-10-04 20:49:42 -0300

    ADD: maquetado de home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F1E3D3",
+        secondary: "#B3B792",
+        accent: "#C58C6D",
+        "secondary-accent": "#725C3A",
+      },
+    },
+  },
+  plugins: [],
+};
