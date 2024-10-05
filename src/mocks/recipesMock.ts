082003f9e33509dbaac710010d4c81b7f3af1983
Lucas Moauro <lmoauro6@gmail.com>
2024-10-05 19:48:13 -0300

    ADD: Mock de recetas

---
+++ src/mocks/recipesMock.ts
@@ -0,0 +1,22 @@
+export const recipeMock = [
+  {
+    img: "../../assets/images/placeholder.jpeg",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi earum obcaecati animi quae, fugit quibusdam quo porro eum libero ullam error quisquam cupiditate fugiat assumenda molestias repellendus laborum fuga architecto?",
+  },
+  {
+    img: "../../assets/images/placeholder.jpeg",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi earum obcaecati animi quae, fugit quibusdam quo porro eum libero ullam error quisquam cupiditate fugiat assumenda molestias repellendus laborum fuga architecto?",
+  },
+  {
+    img: "../../assets/images/placeholder.jpeg",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi earum obcaecati animi quae, fugit quibusdam quo porro eum libero ullam error quisquam cupiditate fugiat assumenda molestias repellendus laborum fuga architecto?",
+  },
+  {
+    img: "../../assets/images/placeholder.jpeg",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi earum obcaecati animi quae, fugit quibusdam quo porro eum libero ullam error quisquam cupiditate fugiat assumenda molestias repellendus laborum fuga architecto?",
+  },
+  {
+    img: "../../assets/images/placeholder.jpeg",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi earum obcaecati animi quae, fugit quibusdam quo porro eum libero ullam error quisquam cupiditate fugiat assumenda molestias repellendus laborum fuga architecto?",
+  },
+];
